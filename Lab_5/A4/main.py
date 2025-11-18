6a91f262dc31a6de740cf55b85a6be20629f5740
Misha <sirakovmihail3@gmail.com>
2025-11-18 13:03:24 +0300

    осталось доредоктировать 2 комманды + прописать коменты и какаято ересь в 1ом

--- Lab_5/A4/main.py
+++ Lab_5/A4/main.py
@@ -16,24 +16,46 @@ with open(f"sequences.{num_for_open}.txt", "r") as text:
         data_map.append([name1, name2, code])
         name = text.readline()
         code = text.readline()
+
+
 num_com = 0
+
+
 with open("ans.txt", "w") as file:
-    with open(f"commands.{num_for_open}.txt", "r") as text:
-        command = text.readline().split()
+    file.write("Dwight Barnette\nGenetic Searching\n")
+    file.write("--------------------------------------------------------------------------\n")
+    with open(f"get_command.{num_for_open}.txt", "r") as text:
+        stroka_command = text.readline()
+        pos_razdel_dannyh_in_str = stroka_command.find("\t")
+        command = stroka_command.replace(stroka_command[pos_razdel_dannyh_in_str:],"")
+        dannye = stroka_command.replace(stroka_command[:pos_razdel_dannyh_in_str + 1],"")
+        get_command = [command, dannye]
         while len(command) != 0:
             num_com += 1
-            if command[0] == "search":
-                file.write(f"{num_com:>3}    {command[0]}    {command[1]}\n")
+            num_com_for_out = str(num_com / 1000)[-3:]
+            if get_command[0] == "search":
+                file.write(f"{num_com_for_out}    {get_command[0]}    {get_command[1]}")
                 file.write("organism                        protein\n")
                 ans = ""
-                need_find = command[1]
+                need_find = get_command[1][:-1]
+                no_one = True
                 for data in data_map:
                     gen_cod = data[2]
                     if gen_cod.find(need_find) != -1:
-                        ans += f"{data[1]:>16} {data[0]}"
+                        ans += f"{data[1][:-1]:<31} {data[0]}\n"
                         file.write(ans)
-            command = text.readline().split()
+                        no_one = False
+                if no_one:
+                    ans = "NOT FOUND\n"
+                    file.write(ans)
+            elif get_command[0] == "diff":
+                pos_razdel_name = get_command[0]
+
+                name1 =
 
 
-
-        command = text.readline().split()
+            stroka_command = text.readline()
+            pos_razdel_dannyh_in_str = stroka_command.find("\t")
+            command = stroka_command.replace(stroka_command[pos_razdel_dannyh_in_str:], "")
+            dannye = stroka_command.replace(stroka_command[:pos_razdel_dannyh_in_str + 1], "")
+            get_command = [command, dannye]
