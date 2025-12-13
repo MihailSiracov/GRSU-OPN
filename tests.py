3e8883f21a23dd839b9330db876495903b46349d
Misha <sirakovmihail3@gmail.com>
2025-12-13 19:41:19 +0300

    закончил комиты 5ой лабы

--- tests.py
+++ tests.py
@@ -1,3 +1,9 @@
-d = [[11, 34], [11, 48]]
-d.remove([11, 34])
-print(d)
+text = "ghbj) (22(33)44)uvhhjb("
+st_br_pos = text.find("(")
+st_poss = []
+
+while st_br_pos != -1:
+    st_poss.append(st_br_pos)
+    text= text[:st_br_pos] + " "+text[st_br_pos+1:]
+    st_br_pos = text.find("(")
+    print( text)
